bc0fb25611537fa5d8fe0f5769e1c7ef18f3cec8
Haqi0 <57205872+Haqi0@users.noreply.github.com>
2020-01-07 15:38:19 +0700

    Add files via upload

---
+++ rentalmobil.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 07, 2020 at 07:42 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rentalmobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_customer` int(20) NOT NULL,
+  `nama_customer` varchar(100) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `no_tlp` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `nama_karyawan` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` int(20) NOT NULL,
+  `no_plat` varchar(100) DEFAULT NULL,
+  `jenis` varchar(100) DEFAULT NULL,
+  `merk` varchar(100) DEFAULT NULL,
+  `tahun` int(4) DEFAULT NULL,
+  `warna` varchar(100) DEFAULT NULL,
+  `harga` int(20) DEFAULT NULL,
+  `stok` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sewa`
+--
+
+CREATE TABLE `sewa` (
+  `id_sewa` int(20) NOT NULL,
+  `id_karyawan` int(20) DEFAULT NULL,
+  `id_customer` int(20) DEFAULT NULL,
+  `id_mobil` int(20) DEFAULT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `total_hari` int(20) DEFAULT NULL,
+  `total_bayar` int(20) DEFAULT NULL,
+  `qty` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `sewa`
+--
+DELIMITER $$
+CREATE TRIGGER `setsewa` AFTER INSERT ON `sewa` FOR EACH ROW BEGIN
+UPDATE mobil SET stok = stok - new.qty
+WHERE id_mobil = new.id_mobil;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tmp` AFTER INSERT ON `sewa` FOR EACH ROW BEGIN
+INSERT INTO `tmp`(`id_sewa`, `id_karyawan`, `id_customer`, `id_mobil`, `tgl_pinjam`, `tgl_kembali`, `total_hari`, `total_bayar`, `qty`) VALUES (new.id_sewa, new.id_karyawan, new.id_customer, new.id_mobil, new.tgl_pinjam, new.tgl_kembali, new.total_hari, new.total_bayar, new.qty);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmp`
+--
+
+CREATE TABLE `tmp` (
+  `id_sewa` int(20) DEFAULT NULL,
+  `id_karyawan` int(20) DEFAULT NULL,
+  `id_customer` int(20) DEFAULT NULL,
+  `id_mobil` int(20) DEFAULT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `total_hari` int(20) DEFAULT NULL,
+  `total_bayar` int(20) DEFAULT NULL,
+  `qty` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `sewa`
+--
+ALTER TABLE `sewa`
+  ADD PRIMARY KEY (`id_sewa`),
+  ADD KEY `id_karyawan` (`id_karyawan`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_mobil` (`id_mobil`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id_customer` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `sewa`
+--
+ALTER TABLE `sewa`
+  MODIFY `id_sewa` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sewa`
+--
+ALTER TABLE `sewa`
+  ADD CONSTRAINT `sewa_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sewa_ibfk_2` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sewa_ibfk_3` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
